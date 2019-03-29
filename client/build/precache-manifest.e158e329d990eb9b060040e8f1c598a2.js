fb4112479775eff668b1771d6ce8d3a32e681853
Anna <a.gondring@gmail.com>
2019-03-28 21:40:51 -0400

    Initial commit

---
+++ client/build/precache-manifest.e158e329d990eb9b060040e8f1c598a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7a48a8d31e0ed20f8e9",
+    "url": "/static/css/main.c94d6dd7.chunk.css"
+  },
+  {
+    "revision": "c7a48a8d31e0ed20f8e9",
+    "url": "/static/js/main.c7a48a8d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "31b9ec7e5be9ab8bf217f07e21b735bf",
+    "url": "/static/media/tgtsnbn.31b9ec7e.jpg"
+  },
+  {
+    "revision": "d223075c790e57612e43d639756539eb",
+    "url": "/index.html"
+  }
+];
